ed1cd86f341118162d7c62bcedd0b9f9538b48c3
rishaviiitdmj <64660727+rishaviiitdmj@users.noreply.github.com>
2022-10-19 02:52:06 +0530

    Update pastinterview.php

--- interview/pastinterview.php
+++ interview/pastinterview.php
@@ -4,12 +4,14 @@ $link = new mysqli('localhost', 'root', '','interview');
 if(!$link) {
 echo '<h4><center>Unable to connect to server.<br>'.mysql_error().'</center></h4>';
 }
+date_default_timezone_set('Asia/Kolkata');
 $dt=date('Y-m-d');
 $query ="SELECT * FROM interviews where doi<'$dt' ORDER BY doi DESC";
 $result = mysqli_query($link, $query);
 
 echo'<center><h1>Interview Scheduler</h1>
-<h2> <a href="home.php">Upcoming Interview</a><br>
+<h2> <a href="joininterview.php">Join Interview</a><br>
+<a href="home.php">Upcoming Interview</a><br>
 <a href="scheduleinterview.html">Schedule Interview</a></h2><br>
 <h3><center>Past Interviews<br></h3>';
 while($row = mysqli_fetch_assoc($result)) {
