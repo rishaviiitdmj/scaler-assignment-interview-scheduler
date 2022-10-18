5b778130348af0bf2b2dc94cca3d09e00b87c33f
rishaviiitdmj <64660727+rishaviiitdmj@users.noreply.github.com>
2022-10-19 02:51:13 +0530

    Update home.php

--- interview/home.php
+++ interview/home.php
@@ -15,26 +15,28 @@
 $link = new mysqli('localhost', 'root', '','interview');
 if(!$link) {
 echo '<h4><center>Unable to connect to server.<br>'.mysql_error().'</center></h4>';
-}else{
+}
+else{
+date_default_timezone_set('Asia/Kolkata');
 $dt=date('Y-m-d');
-$query ="SELECT * FROM interviews where doi>='$dt' ORDER BY doi";
+$query ="SELECT * FROM interviews where doi>'$dt' ORDER BY doi";
 $result = mysqli_query($link, $query);
 
 echo'<center><h1>Interview Scheduler</h1>
-<h2> <a href="pastinterview.php">Past Interview</a><br>
+<h2> <a href="joininterview.php">Join Interview</a><br>
+<a href="pastinterview.php">Past Interview</a><br>
 <a href="scheduleinterview.html">Schedule Interview</a></h2><br>
 <h3>Upcoming Interviews<br></h3>';
 while($row = mysqli_fetch_assoc($result)) {
     echo "<div><br>Company Name: " . $row["name"]. "<br>CTC: " . $row["ctc"]. "<br>Description: ".$row["description"].
 	"<br>Created on: ".$row["createdon"]."<br>Date of Interview ".$row["doi"]."<br><br></div>";
-	if($dt<$row["doi"]){
 	?>
 
 <button onclick="document.getElementById('id01').style.display='block'">Add Partipants</button>
 <div id="id01" class="modal">
   <span onclick="document.getElementById('id01').style.display='none'"class="close" title="Close Modal">&times;</span>
   <form class="modal-content animate"  method="post" action="addnew.php">
-     <?php echo '<input type="number" name="cid" value="'.$row["cid"].'" required><br>'?>
+     <?php echo '<input type="hidden" name="cid" value="'.$row["cid"].'" required><br>'?>
      Your Name:- <input type="text" name="name" required><br>
 	 Email:- <input type="email" name="mail" required><br>
      College Name:- <input type="text" name="college" required><br>
@@ -46,27 +48,8 @@ while($row = mysqli_fetch_assoc($result)) {
   </form>
 </div>
 
-
 <?php
 	}
-	else if($dt==$row["doi"]){
-	
-		?>
-		
-<button onclick="document.getElementById('id01').style.display='block'">Join Interview</button>
-<div id="id01" class="modal">
-  <span onclick="document.getElementById('id01').style.display='none'"class="close" title="Close Modal">&times;</span>
-  <form class="modal-content animate" action="join.php" method="post">
-     <?php echo '<input type="hidden" name="cid" value="'.$row["cid"].'"required><br>'?>
-	 Email:- <input type="email" name="mail" required><br>
-	<input type="submit" value="Join"><br>
-     <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-  </form>
-</div>
-	
-<?php	
-	}
   }
-}
 mysqli_close($link);
 ?>
